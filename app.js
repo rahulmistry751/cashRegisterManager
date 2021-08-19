f7c32d2066aa0eaf652b5be5cce187c55216389e
rahulmistry751 <rahulmistry751@gmail.com>
2021-08-20 01:24:21 +0530

    fix:correct input value type coercion

--- app.js
+++ app.js
@@ -7,40 +7,44 @@ const returnAmount=document.querySelector(".return-am");
 const paidSection=document.querySelector(".paid-section");
 const numOfNotes=document.querySelectorAll(".numOfNotes");
 const warning=document.querySelector(".warning");
-
+//
+let paid=0;let bill=0;
 // Event listeners
 checkBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    //console.log(paidAmount.value);
-    if(!paidAmount.value){ // checks if paid amount is entered
+    paid=Number(paidAmount.value);
+    //console.log(paid);
+    if(!paid){ // checks if paid amount is entered
         returnAmount.style.color="red";
         returnAmount.innerText="Please enter a paid amount";
     }
     
-    else if(billAmount.value===paidAmount.value){ //Checks if return change is 0
+    else if(bill===paid){ //Checks if return change is 0
         returnAmount.style.color="Black";
         returnAmount.innerHTML=`<h3>All is settled</h3>`;
         clearnotes();
     }
-    else if(billAmount.value>paidAmount.value){   //Checks if paid amount is valid
+    else if(bill>paid){   //Checks if paid amount is valid
         returnAmount.style.color="red";
         returnAmount.innerText="Paid amount is lesser than Bill amount, Please enter valid Paid amount";
         clearnotes();
     }
     else{
         returnAmount.style.color="Black";
-        returnAmount.innerHTML=`<h3>Return change: ${paidAmount.value-billAmount.value}</h3>`;
+        returnAmount.innerHTML=`<h3>Return change: ${paid-bill}</h3>`;
         clearnotes();
-        calculateChange();
+        calculateChange(paid,bill);
     }
     
-    //console.log(billAmount.value);
-//console.log(paidAmount.value);
+    //console.log(bill.value);
+//console.log(paid.value);
 })
 nextBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    //console.log(billAmount.value);
-    if(billAmount.value && billAmount.value!=0){  //Checks for valid bill amount
+    
+    bill=Number(billAmount.value);
+    //console.log(bill);
+    if(bill&& bill!=0){  //Checks for valid bill amount
         warning.innerText="";
         paidSection.classList.add("active");
     }
@@ -51,14 +55,14 @@ nextBtn.addEventListener("click",(e)=>{
     
 })
 
-function calculateChange(){
+function calculateChange(paid,bill){
    
     let notes=[2000,500,100,20,10,5,1];
-    let obj={};
-    let temp=paidAmount.value-billAmount.value;  
+    let temp=paid-bill;  
     for(i=0;i<notes.length;i++){
         if(temp>=notes[i]){    // Checks for notes smaller than the change value
             // Saves the number of notes needs for a particular note
+            console.log(temp);
             numOfNotes[i].innerText=Math.floor(temp/notes[i]);   
             if(Math.floor(temp/notes[i])===0){  //Checks if change is settled
                 break;
